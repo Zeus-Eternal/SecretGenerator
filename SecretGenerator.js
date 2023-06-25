1d27953e668f8e7e19343a33cdc3ab7847b073b1
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-06-25 08:04:00 -0400

    Create SecretGenerator.js

---
+++ SecretGenerator.js
@@ -0,0 +1,53 @@
+const crypto = require('crypto');
+
+/**
+ * Generates a random secret key.
+ * @param {number} length - The length of the secret key in bits.
+ * @returns {string} The generated secret key as a hexadecimal string.
+ */
+const generateSecretKey = (length) => {
+  const byteLength = Math.ceil(length / 8);
+  const buffer = crypto.randomBytes(byteLength);
+  return buffer.toString('hex');
+};
+
+/**
+ * Generates multiple random secret keys.
+ * @param {number} count - The number of secret keys to generate.
+ * @param {number} length - The length of each secret key in bits.
+ * @returns {string[]} An array of generated secret keys.
+ */
+const generateSecretKeys = (count, length) => {
+  const secretKeys = [];
+  for (let i = 0; i < count; i++) {
+    const secretKey = generateSecretKey(length);
+    secretKeys.push(secretKey);
+  }
+  return secretKeys;
+};
+
+// Check if the script is executed from the command line
+if (require.main === module) {
+  // If executed from the command line, generate the secret keys
+  const [length, count] = process.argv.slice(2);
+
+  const keyLength = length ? parseInt(length, 10) : 256;
+  const keyCount = count ? parseInt(count, 10) : 1;
+
+  if (isNaN(keyLength) || isNaN(keyCount)) {
+    console.error('Invalid command-line arguments. Please provide valid numeric values for length and count.');
+    process.exit(1);
+  }
+
+  const secretKeys = generateSecretKeys(keyCount, keyLength);
+
+  console.log(`Generated ${keyCount} secret key(s) of length ${keyLength} bits:`);
+  for (let i = 0; i < secretKeys.length; i++) {
+    console.log(`Secret Key ${i + 1}: ${secretKeys[i]}`);
+  }
+}
+
+module.exports = {
+  generateSecretKey,
+  generateSecretKeys,
+};
